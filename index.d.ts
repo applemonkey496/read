76bdb09bc4d97e1a80cc37be028b521bf7d00702
applemonkey496 <55333787+applemonkey496@users.noreply.github.com>
2021-03-11 17:25:46 -0800

    add ts declarations

---
+++ index.d.ts
@@ -0,0 +1,84 @@
+
+import type { Interface } from 'readline'
+
+declare function Read(options: Read.Options, callback: Read.Callback): void
+declare function Read(options: Read.Options): Promise<void>
+declare function Read<T>(options: Read.Options): Promise<T>
+
+declare namespace Read {
+    type Callback = (error: any, value: string, isDefault: boolean) => void
+
+    interface Options {
+        /** 
+         * The default value to pass if the input is 
+         * empty. `isDefault` will be set to `true` if
+         * this happens.
+         */
+        default?: string,
+
+        /** Allow the user to change the default value */
+        edit?: boolean,
+
+        /** Prints this before reading input. */
+        prompt?: string,
+
+        /** The input stream. */
+        input?: ReadableStream,
+
+        /** The output stream. */
+        output?: WritableStream,
+
+        /** 
+         * The character(s) to used instead of silenced 
+         * characters. Requires `silent` to be `true`. 
+         * 
+         * @example
+         * // Suppress password characters with '*'
+         * const options = {
+         *     prompt: 'Password: ',
+         *     silent: true,
+         *     replace: '*'
+         * }
+         */
+        replace?: string,
+
+        /**
+         * The function to run when a SIGINT (Ctrl-C
+         * or equivalent) is sent. By default, this 
+         * closes the `readline.Interface` instance 
+         * and throws an error.
+         * 
+         * @param rl The `readline.Interface` used
+         * by `read`. 
+         * 
+         * @example
+         * function sigintHandler(rl: Interface): void {
+         *     rl.close();
+         *     console.log('Caught SIGINT');
+         *     process.exit(1);
+         * }
+         */
+        sigintHandler: (rl: Interface) => void,
+
+        /** Whether to suppress input. */
+        silent?: boolean,
+
+        /** 
+         * Number of milliseconds to wait before 
+         * canceling. After this amount of time,
+         * an error will be thrown.
+         */
+        timeout?: number,
+
+        /** 
+         * Whether to assume the output is a TTY.
+         * If set to `true`, `read` will assume it
+         * is a TTY, whether it actually is or not.
+         * If set to `false` or left empty, `read` 
+         * will infer based on `output.isTTY`.
+         */
+        terminal?: boolean
+    }
+}
+
+export = Read
